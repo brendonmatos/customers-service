49005b3bd1bdee23190f0fa1f280b42f8baa68fd
brendonmatos <brendonferreiradm@gmail.com>
2022-05-29 16:17:52 -0300

    test: add initial e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,10 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+
+const uuidRegexExp = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
+
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
@@ -21,4 +25,38 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('/customers (POST)', async () => {
+    const test = await request(app.getHttpServer())
+      .post('/customers')
+      .send({
+        name: 'John Doe',
+        document: '12345678910',
+      })
+
+    expect(test.status).toBe(201);
+    expect(test.body).toHaveProperty('id');
+    expect(test.body.id).toMatch(uuidRegexExp);
+    expect(test.body).toHaveProperty('name', 'John Doe');
+    expect(test.body).toHaveProperty('document', '12345678910');
+  })
+
+  it('/customers/:id (GET)', async () => {
+    const createTest = await request(app.getHttpServer())
+      .post('/customers')
+      .send({
+        name: 'John Doe',
+        document: '12345678910',
+      })
+
+    const getTest = await request(app.getHttpServer())
+      .get('/customers/' + createTest.body.id)
+
+    expect(getTest.status).toBe(200);
+    expect(getTest.body).toHaveLength(1);
+    expect(getTest.body[0]).toHaveProperty('name', 'John Doe');
+    expect(getTest.body[0]).toHaveProperty('document', '12345678910');
+  })
+
+
 });
